c6f82821fce128eace4e8d909f7ddd468a57a486
bluezip <serin212@hotmail.com>
2014-08-05 16:01:11 +0700

    Unit test

--- test/index.js
+++ test/index.js
@@ -1,6 +1,27 @@
-var pantip    = require('../index');
+var pantip    = require('../src/index');
+var should    = require('should');
 
-pantip.load('http://pantip.com/topic/32414312',function(err,json){
-  console.log(json);
-  console.log(err);
+
+describe('Pantip', function(){
+  this.timeout(120 * 1000);
+
+  it(':page', function(done){
+    pantip.page('http://pantip.com/topic/32414312',function(err,data){
+
+      data.title.should.be.type('string');
+      data.description.should.be.type('string');
+      data.keyword.should.be.an.Array;
+      data.comment.should.be.an.Array;
+
+      if(err){ done(err); }else{ done(); }
+    });
+  });
+
+  it(':forum', function(done){
+    pantip.forum('http://pantip.com/forum/food',function(err,data){
+      data.should.be.an.Array;
+      console.log(data);
+      if(err){ done(err); }else{ done(); }
+    });
+  });
 });
